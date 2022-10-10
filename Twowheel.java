f72106f9a57be65ddd037cb967b0af0373210682
Nithi4623 <115499840+Nithi4623@users.noreply.github.com>
2022-10-11 00:25:27 +0530

    Add files via upload

---
+++ Twowheel.java
@@ -0,0 +1,23 @@
+package zithi;
+
+public class Twowheel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int noOfWheels=2;
+		System.out.println(noOfWheels);
+        short noOfMirrors=2;
+        System.out.println(noOfMirrors);
+        long chassisNumber=1234567890123L;
+        System.out.println(chassisNumber);
+        boolean isPunctured=false;
+        System.out.println(isPunctured);
+        String bikeName="RX";
+        System.out.println(bikeName);
+        double runningKilometer=999;
+        System.out.println(runningKilometer);
+        
+
+	}
+
+}
---
+++ mobile.java
@@ -0,0 +1,26 @@
+package zithi;
+
+public class mobile {
+	String mobilename="oppo";
+    char mobilelog='o';
+    short noOfmobile=3;
+    int modelnumber=19;
+    long mobileiminumber=56547573257l;
+    float mobileprice=13000;
+    boolean isdamaged=false;
+    
+      
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       mobile mob=new mobile();
+       System.out.println(mob.mobilename);
+       System.out.println(mob.mobilelog);
+       System.out.println(mob.noOfmobile);
+       System.out.println(mob.modelnumber);
+       System.out.println(mob.mobileiminumber);
+       System.out.println(mob.mobileprice);
+       System.out.println(mob.isdamaged);
+           		   
+	}
+
+}
